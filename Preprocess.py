51d5733129d5c846c47c65f09f9e440cbbf7e7da
wngud1227 <76057456+wngud1227@users.noreply.github.com>
2021-03-19 14:07:25 +0900

    Add files via upload

---
+++ Preprocess.py
@@ -0,0 +1,132 @@
+import sys
+sys.path.append('..')
+import collections
+import zipfile
+import numpy as np
+
+class vocab_word:
+    def __init__(self, word):
+        self.count = 0
+        self.word = word
+        self.code = 0
+        self.codelen = 0
+
+class preprocess:
+    def __init__(self, file):
+        self.file = file
+        self.dictionary_size = 1000
+        self.min_count = 5
+        self.vocab_size = 0
+        self.vocab_max_size = 1000
+        self.Max_string = 50
+        self.vocab = list(-1 for i in range(self.vocab_max_size))
+
+    def readzip(self):
+        with zipfile.ZipFile(self.file) as f:
+            names = f.namelist()
+            contents = f.read(names[0])
+            text = contents.split()
+            return text
+
+
+    def readtext(self):
+        newfile = []
+        with open(self.file, 'r', encoding='UTF8') as f:
+            for text in f.readlines():
+                text = text.replace(' .', ' <EOS>')
+                text = text.lower().split()
+                for word in text:
+                    if len(word) > self.Max_string:
+                        text.remove(word)
+                newfile.extend(text)
+            return newfile
+
+    def wordset(self):
+        text = self.readtext()
+        unique = collections.Counter(text)
+        orders = unique.most_common()
+        for word in orders:
+            if word[1] < self.min_count:
+                del word
+        count = [['<UNK>', -1]]
+        count.extend(orders)
+        dictionary = []
+        i = 0
+        for word, _ in count:
+            dictionary[i] = word
+
+
+        data = []
+        for word in text:
+            if word in dictionary:
+                index = dictionary[word]
+            else:
+                index = 0
+                count[0][1] += 1
+            data.append(index)
+        return data, count
+
+    # def Huffman_coding(self):
+    #     vocab_size = len(self.wordset())
+    #     pos1 =
+    #     for i in range(len(self.wordset()) - 1):
+    #         if
+
+class UnigramSampler:
+    def __init__(self, counts, power, sample_size):
+        self.sample_size = sample_size
+        self.vocab_size = len(counts)
+        self.word_p = np.zeros(self.vocab_size)
+        self.word_p = np.power(counts, power)
+        self.word_p /= np.sum(self.word_p)
+
+    def get_negative_sample(self, target):
+        batch_size = target.shape[0]
+        negative_sample = np.zeros((batch_size, self.sample_size), dtype=np.int32)
+
+        for i in range(batch_size):
+            p = self.word_p.copy()
+            target_idx = target[i]
+            p[target_idx] = 0
+            p /= p.sum()
+            negative_sample[i, :] = np.random.choice(self.vocab_size, size=self.sample_size, replace=False, p=p)
+
+        return negative_sample
+
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+
+    batch_size = y.shape[0]
+
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+
+class SigmoidWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = 1 / (1 + np.exp(-x))
+
+        self.loss = cross_entropy_error(np.c_[1 - self.y, self.y], self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = (self.y - self.t) * dout / batch_size
+        return dx
+
+
+
+
---
+++ word2vec.py
@@ -0,0 +1,208 @@
+import sys
+sys.path.append('..')
+import numpy as np
+import time
+from Preprocess import preprocess, UnigramSampler, SigmoidWithLoss
+
+
+def create_contexts_target(corpus, window_size=5):
+    target = corpus[window_size:-window_size]
+    contexts = []
+
+    for idx in range(window_size, len(corpus)-window_size):
+        cs = []
+        for t in range(-window_size, window_size + 1):
+            if t == 0:
+                continue
+            cs.append(corpus[idx + t])
+        contexts.append(cs)
+
+    return np.array(contexts), np.array(target)
+
+def remove_duplicate(params, grads):
+    params, grads = params[:], grads[:]
+
+    while True:
+        find_flg = False
+        L = len(params)
+
+        for i in range(0, L - 1):
+            for j in range(i + 1, L):
+
+                if params[i] is params[j]:
+                    grads[i] += grads[j]
+                    find_flg = True
+                    params.pop(j)
+                    grads.pop(j)
+
+                elif params[i].ndim == 2 and params[j].ndim == 2 and \
+                     params[i].T.shape == params[j].shape and np.all(params[i].T == params[j]):
+                    grads[i] += grads[j].T
+                    find_flg = True
+                    params.pop(j)
+                    grads.pop(j)
+
+                if find_flg: break
+            if find_flg: break
+
+        if not find_flg: break
+
+    return params, grads
+
+class Embedding:
+    def __init__(self, W):
+        self.params = W
+        self.grads = np.zeros_like(W)
+        self.idx = None
+
+    def forward(self, idx):
+        W = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+
+    def backward(self, dout):
+        dW = self.grads
+        dW[...] = 0
+        for i, word_id in enumerate(self.idx):
+            dW[word_id] += dout[i]
+        return None
+
+class EmbeddingDot:
+    def __init__(self, W):
+        self.embed = Embedding(W)
+        self.params = self.embed.params
+        self.grads = self.embed.grads
+        self.cache = None
+
+    def forward(self, h, idx):
+        target_W = self.embed.forward(idx)
+        out = np.sum(target_W * h, axis=1)
+
+        self.cache = (h, target_W)
+        return out
+
+    def backward(self, dout):
+        h, target_W = self.cache
+        dout = dout.reshape(dout.shape[0], 1)
+        dtarget_W = dout * h
+        self.embed.backward(dtarget_W)
+        dh = dout * target_W
+        return dh
+
+class NegativeSamplingLoss:
+    def __init__(self, W, corpus, power=0.75, sample_size=5):
+        self.sample_size = sample_size
+        self.sampler = UnigramSampler(corpus, power, sample_size)
+        self.loss_layers = [SigmoidWithLoss() for _ in range(sample_size + 1)]
+        self.embed_dot_layers = [EmbeddingDot(W) for _ in range(sample_size + 1)]
+        self.params, self.grads = [0], [0]
+        for layer in self.embed_dot_layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+    def forward(self, h, target):
+        batch_size = target.shape[0]
+        negative_sample = self.sampler.get_negative_sample(target)
+
+        score = self.embed_dot_layers[0].forward(h, target)
+        correct_label = np.ones(batch_size, dtype=np.int32)
+        loss = self.loss_layers[0].forward(score, correct_label)
+
+        negative_label = np.zeros(batch_size, dtype=np.int32)
+        for i in range(self.sample_size):
+            negative_target = negative_sample[:, i]
+            score = self.embed_dot_layers[1 + i].forward(h, negative_target)
+            loss += self.loss_layers[1 + i].forward(score, negative_label)
+
+        return loss
+
+    def backward(self, dout=1):
+        dh = 0
+        for l0, l1 in zip(self.loss_layers, self.embed_dot_layers):
+            dscore = l0.backward(dout)
+            dh += l1.backward(dscore)
+
+        return dh
+
+class CBOW:
+    def __init__(self, window_size, negative_sample, corpus, W_in, W_out):
+        if negative_sample:
+            self.W_embedding = W_in
+            self.W_embedding_b = W_out
+
+            self.in_layers = []
+            for i in range(2 * window_size):
+                layer = Embedding(W_embedding)
+                self.in_layers.append(layer)
+            self.ns_loss = NegativeSamplingLoss(W_embedding_b, corpus, power=0.75, sample_size=5)
+
+            layers = self.in_layers + [self.ns_loss]
+            self.params, self.grads = [], []
+            for layer in layers:
+                self.params += layer.params
+                self.grads += layer.grads
+
+
+
+        # else:
+        #     W_embedding = 0.01 * np.random.randn(V, H).astype('f')
+        #     W_embedding_b = 0.01 * np.random.randn(H, V).astype('f')
+
+    def forward(self, contexts, target):
+        h = 0
+        for i, layer in enumerate(self.in_layers):
+            h += layer.forward(contexts[:, i])
+        h *= 1 / len(self.in_layers)
+        loss = self.ns_loss.forward(h, target)
+        return loss
+
+    def backward(self, dout=1):
+        dout = self.ns_loss.backward(dout)
+        dout *= 1 / len(self.in_layers)
+        for layer in self.in_layers:
+            layer.backward(dout)
+        return None
+
+max_epoch = 6
+max_window_size = 5
+hidden_unit = 100
+batch_size = 100
+
+
+preprocess = preprocess('data/news1.txt')
+data, vocab = preprocess.wordset()
+vocab_size = len(vocab)
+start_time = time.time()
+
+W_embedding = 0.01 * np.random.randn(vocab_size, hidden_unit).astype('f')
+W_embedding_b = 0.01 * np.random.randn(hidden_unit, 1).astype('f')
+total_loss, loss_count = 0, 0
+for epoch in range(max_epoch):
+    window_size = np.random.randint(1, max_window_size)
+    contexts, target = create_contexts_target(data, window_size)
+    model = CBOW(window_size, negative_sample=True, corpus=data, W_in=W_embedding, W_out=W_embedding_b)
+
+    max_iter = len(contexts) // batch_size
+    idx = np.random.permutation(np.arange(len(contexts)))
+    x = contexts[idx]
+    t = target[idx]
+
+    for iter in range(max_iter):
+        batch_x = x[iter * batch_size:(iter + 1) * batch_size]
+        batch_t = t[iter * batch_size:(iter + 1) * batch_size]
+        loss = model.forward(batch_x, batch_t)
+        model.backward()
+        params, grads = remove_duplicate(model.params, model.grads)
+
+        total_loss += loss
+        loss_count += 1
+
+        elapsed_time = time.time() - start_time
+        print('Epoch: {}, Iteration: {} / {}, Time: {}, loss: {}'.format(epoch + 1, iter + 1, max_iter, elapsed_time, total_loss / loss_count))
+
+    W_embedding = model.W_embedding
+    W_embedding_b = model.W_embedding_b
+
+
+
