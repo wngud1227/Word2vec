907a0c438f908f1962b816965ae315b919248d4b
wngud1227 <76057456+wngud1227@users.noreply.github.com>
2022-01-11 18:38:22 +0900

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,10 @@ from collections import Counter
 import pickle
 import time
 
+def sigmoid(x):
+    out = 1./(1.+np.exp(-x))
+    return out
+
 def readtext(file):
     with open(file, 'r', encoding='utf-8') as f:
         data = f.read()
@@ -41,7 +45,7 @@ def wordset(count): #count: 최소 빈도수, vocabulary만들기
 
     return word_to_id, id_to_word, vocab
 
-def textfile(word_to_id):
+def textfile(word_to_id, new_file = './data/news1_processed.txt'):
     text = []
     file = './data/news1.txt'
     words = readtext(file)
@@ -56,31 +60,47 @@ def textfile(word_to_id):
         else: sentence.append(word_to_id['<UNK>'])
 
     #file 저장
-    new_file = './data/news1_processed.txt'
     with open(new_file, 'wb') as f:
         pickle.dump(text, f)
 
+def create_contexts_target(corpus, window_size=1):
+    target = corpus[window_size:-window_size]
+    contexts = []
+
+    for idx in range(window_size, len(corpus)-window_size):
+        cs = []
+        for t in range(-window_size, window_size + 1):
+            if t == 0:
+                continue
+            cs.append(corpus[idx + t])
+        contexts.append(cs)
+
+    return contexts, target
 
 # (word_to_id, id_to_word, count) = wordset(5)
-# with open('./news/vocab.txt', 'wb') as v:
+# textfile(word_to_id)
+# with open('./data/vocab.txt', 'wb') as v:
 #     pickle.dump((word_to_id, id_to_word, count), v)
 
 class Node:
-    def __init__(self, count, word, left=None, right=None):
+    def __init__(self, count, id, symbol=None, left=None, right=None):
         self.count = count
-        self.word = word
+        self.id = id
+        self.symbol = symbol
         self.left = left
         self.right = right
 
 def tree(vocab, id_to_word):
     nodes = []
     for i in range(len(id_to_word)):
-        node = Node(vocab[id_to_word[i]], id_to_word[i])
+        node = Node(vocab[id_to_word[i]], i)
         nodes.append(node)
+    symbol = 0
     while len(nodes) > 1:
         left = nodes[-1]
         right = nodes[-2]
-        node = Node(left.count + right.count, None, left, right)
+        node = Node(left.count + right.count, None, symbol, left, right)
+        symbol += 1
         nodes.remove(left)
         nodes.remove(right)
         leng = len(nodes)
@@ -92,27 +112,82 @@ def tree(vocab, id_to_word):
             nodes.insert(0, node)
     return nodes[0]
 
-def huffman(node, code, codes):
-    if(node.left):
-        code += 0
-        huffman(node.left, code, codes)
-    if(node.right):
-        code += 1
-        huffman(node.right, code, codes)
+def huffman(node, code=[], id_to_code={}, id_to_way={}, way=[]):
+    if(node.symbol):
+        if(node.left):
+            way.append(node.symbol)
+            code.append(1)
+            huffman(node.left, code, id_to_code, id_to_way, way)
+        if(node.right):
+            way.append(node.symbol)
+            code.append(-1)
+            huffman(node.right, code, id_to_code, id_to_way, way)
+    
     else:
-        codes[node.word] = code
+        id_to_code[node.id] = code
+        id_to_way[node.id] = way
 
-    return codes
+    return id_to_code, id_to_way
 
-with open('./news/vocab.txt', 'rb') as v:
+with open('./data/vocab.txt', 'rb') as v:
     (word_to_id, id_to_word, count) = pickle.load(v)
     start = time.time()
     node = tree(count, id_to_word)
     print('tree end : {}'.format((time.time()-start)/60))
+#
+id_to_code, id_to_way = huffman(node)
 
-code = ''
-codes = {}
-codes = huffman(node, code, codes)
-print(codes)
-print('encoding end')
+max = 0
+for code in id_to_code.values():
+    if max < len(code):
+        max = len(code)
 
+l = len(id_to_code)
+mat_code = np.zeros((l,max))
+mat_way = np.zeros((l,max))
+for id in range(l):
+    code = id_to_code[id]
+    way = id_to_way[id]
+
+    if max > len(code):
+        code[len(code):max] = 0
+        way[len(way):max] = 0
+
+    mat_code[id] = code
+    mat_way[id] = way
+
+print('code : {}, way: {}'.format(mat_code.shape, mat_way.shape))
+
+with open('./data/huffman.txt', 'wb') as hf:
+    pickle.dump((mat_code, mat_way), hf)
+    print('encoding end : {}'.format((time.time()-start)/60))
+
+
+# with open('./data/huffman.txt', 'rb') as hf:
+#     (id_to_code, id_to_way) = pickle.load(hf)
+# with open('./data/news1_processed.txt', 'rb') as data:
+#     text = pickle.load(data)
+#
+#     W_embedding = np.random.uniform(low=-0.5, high=0.5, size=(len(id_to_code), 10))
+#     W_embedding_b = np.random.uniform(low=-0.5, high=0.5, size=(len(id_to_code) - 1, 10))
+#
+#     for sentence in text:
+#         contexts, center = create_contexts_target(sentence)
+#         for i in range(len(center)):
+#             loss = 0
+#             for context in contexts[i]:
+#                 way = id_to_way[context]
+#                 code = id_to_code[context]
+#                 v= 1
+#                 for j in range(len(way)):
+#                     score = code[j] * np.dot(W_embedding_b[way[j]], W_embedding[center[i]])
+#
+#
+
+#
+#
+# loss = 0
+# for context in contexts:
+#     x = W_embedding[context]
+#     x = x.reshape(1, 10)
+#     for
