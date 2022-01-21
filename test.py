3e3f45fc499f97d3c388a8d18118658411dc9cc2
wngud1227 <76057456+wngud1227@users.noreply.github.com>
2022-01-21 16:28:46 +0900

    Update test.py

--- test.py
+++ test.py
@@ -10,6 +10,38 @@ import time
 from tqdm.auto import tqdm
 
 
+def remove_duplicate(params, grads):
+    params, grads = params[:], grads[:]  # copy list
+    while True:
+        find_flg = False
+        L = len(params)
+
+        for i in range(0, L - 1):
+            for j in range(i + 1, L):
+                if params[i] is params[j]:
+                    grads[i] += grads[j]
+                    find_flg = True
+                    params.pop(j)
+                    grads.pop(j)
+
+                elif params[i].ndim == 2 and params[j].ndim == 2 and \
+                        params[i].T.shape == params[j].shape and np.all(params[i].T == params[j]):
+                    grads[i] += grads[j].T
+
+                    find_flg = True
+                    params.pop(j)
+                    grads.pop(j)
+
+                if find_flg:
+                    break
+            if find_flg:
+                break
+
+        if not find_flg:
+            break
+
+    return params, grads
+
 def sigmoid(x):
     out = 1. / (1. + np.exp(-x))
     return out
@@ -31,7 +63,7 @@ def create_contexts_target(corpus, window_size=1):
 
 class SkipGram:
     def __init__(self, window_size, hidden_unit): #window_size: the number of input word, hidden_unit : dimension of vector
-        with open('data/dataset/news/vocab.txt', 'rb') as f:
+        with open('news/vocab.txt', 'rb') as f:
             corpus = pickle.load(f)
         self.word_to_id = corpus[0]
         self.id_to_word = corpus[1]
@@ -42,8 +74,8 @@ class SkipGram:
         self.window_size = np.random.randint(1, window_size)
         self.cache = None
 
-        self.W_embedding = np.random.uniform(low=-0.5, high=0.5, size=(self.vocab_size, self.dimension))
-        self.W_embedding_b = np.random.uniform(low=-0.5, high=0.5, size=(self.vocab_size - 1, self.dimension))
+        self.W_embedding = 0.01 * np.random.randn(self.vocab_size, self.dimension).astype('f')
+        self.W_embedding_b = np.zeros((self.vocab_size - 1, self.dimension)).astype('f')
 
 
     def forward(self, contexts, center, id_to_code, id_to_way):
@@ -54,24 +86,30 @@ class SkipGram:
         x = x.reshape(1, self.dimension)
 
         for context in contexts:
-            code = np.array(id_to_code[context]).reshape(1, -1)   #(1, V-1)
+            code = np.array(id_to_code[context]).reshape(-1, 1)   #(V-1, 1)
             way = list(id_to_way[context])  #(V-1,)
-            score = sigmoid(np.dot(code, np.dot(x, self.W_embedding_b[way].T).T)) #(1, 1)
-            loss += -np.log(score.squeeze(0) + 1e-07)
+            score = sigmoid(code * np.dot(self.W_embedding_b[way], x.T)) #(V-1, 1)
+            loss -= np.sum(np.log(score + 1e-07))
             cache.append((x, context, code, way, score))
 
         self.cache = cache
         return float(loss)/len(contexts)
 
     def backward(self, lr):
+        params, grads = [], []
+
         for (x, context, code, way, score) in self.cache:
-            dout = ((score - 1) * code) #(1, V-1)
-            dW_out = np.dot(dout.T, x) #(V-1, D)
-            dx = np.dot(dout, self.W_embedding_b[way]) #(1, D)
-
-            self.W_embedding_b[way] -= dW_out * lr
+            dout = ((score - 1) * code) #(V-1, 1)
+            dW_out = np.dot(dout, x) #(V-1, D)
+            dx = np.dot(dout.T, self.W_embedding_b[way]) #(1, D)
+            params.extend(way)
+            grads.extend(dW_out)
             self.W_embedding[context] -= dx.squeeze() * lr
-
+        params = np.array(params)
+        grads = np.array(grads)
+        params, grads = remove_duplicate(params, grads)
+        self.W_embedding_b[params] -= gra
+        ds * lr
         return None
 
     def train(self, epoch):
@@ -85,48 +123,48 @@ class SkipGram:
             loss = 0
             count = 0
 
-            for i in tqdm(range(100), desc='Iteration'):
-                if i < 10:
-                    num = '0' + str(i)
-                else:
-                    num = str(i)
-                data = 'data/dataset/news/en-000' + num + '-of-00100.txt'
-                with open(data, 'rb') as f:
-                    text = pickle.load(f)
+            # for i in tqdm(range(100), desc='Iteration'):
+            #     if i < 10:
+            #         num = '0' + str(i)
+            #     else:
+            #         num = str(i)
+            data = './news/en-00000-of-00100.txt'
+            with open(data, 'rb') as f:
+                text = pickle.load(f)
 
-                for sentence in text:
-                    n += 1
-                    contexts, target = create_contexts_target(sentence, window_size=self.window_size + 1)
-
-
-                    for i in range(len(contexts)):
-                        count += 1
-                        loss += self.forward(contexts[i], target[i], id_to_code, id_to_way)
-                        self.backward(lr)
-
-                    #lr decay
-                    alpha = 1 - n/(self.num_sentence * epoch)
-                    if alpha <= 0.0001:
-                        alpha = 0.0001
-
-                    lr = self.lr * alpha
+            for sentence in tqdm(text):
+                n += 1
+                contexts, target = create_contexts_target(sentence, window_size=self.window_size + 1)
 
 
-                    if count % 10000 == 1:
-                        train_time = (time.time() - start) / 3600
-                        avg_loss = loss/count
-                        print('time: {}, loss : {}'.format(train_time, avg_loss))
-                        print('{} sentence trained!'.format(n))
-                        print('learning rate : {}'.format(lr))
+                for i in range(len(contexts)):
+                    count += 1
+                    loss += self.forward(contexts[i], target[i], id_to_code, id_to_way)
+                    self.backward(lr)
 
-                        count = 0
-                        loss = 0
+                #lr decay
+                alpha = 1 - n/(self.num_sentence * epoch)
+                if alpha <= 0.0001:
+                    alpha = 0.0001
+
+                lr = self.lr * alpha
+
+
+                if count % 10000 == 1:
+                    train_time = (time.time() - start) / 3600
+                    avg_loss = loss/count
+                    print('time: {}, loss : {}'.format(train_time, avg_loss))
+                    print('{} sentence trained!'.format(n))
+                    print('learning rate : {}'.format(lr))
+
+                    count = 0
+                    loss = 0
 
 
         print('Train Finished!')
         print('Train time : {}'.format(time.time()-start))
 
-        with open('data/dataset/embedding_sg_hf.pkl', 'wb') as f:
+        with open('data/embedding_sg_hf.pkl', 'wb') as f:
             pickle.dump(self.W_embedding, f)
         return None
 
@@ -134,12 +172,12 @@ class SkipGram:
 #
 # # #test
 # save_test(file='./data/test.txt', word_to_id=word_to_id)
-model = SkipGram(window_size=5, hidden_unit=300)
-model.train(epoch=1)
+# model = SkipGram(window_size=5, hidden_unit=300)
+# model.train(epoch=1)
 
-# with open('./data/dataset/embedding_sg_neg15_2.pkl', 'rb') as f:
-#     W_embedding = pickle.load(f)
-# accuracy = accuracy(file='./data/dataset/news/test_labeled.pkl', W=W_embedding)
-# print(accuracy[0], accuracy[1])
+with open('./data/embedding_sg_hf.pkl', 'rb') as f:
+    W_embedding = pickle.load(f)
+accuracy = accuracy(file='./news/test_labeled.pkl', W=W_embedding)
+print(accuracy[0], accuracy[1])
 #
 # print(most_similar('king', W_embedding, word_to_id, id_to_word, top=5))
