e27cabc783189c544660de8ffa6dddf1e4f053d0
wngud1227 <76057456+wngud1227@users.noreply.github.com>
2022-02-15 16:54:45 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,46 +1,9 @@
-import sys
-
-sys.path.append('..')
-
-from evaluation import *
-import numpy as np
 import pickle
+import numpy as np
+from evaluation import *
 import time
-
 from tqdm.auto import tqdm
 
-
-def remove_duplicate(params, grads):
-    params, grads = params[:], grads[:]  # copy list
-    while True:
-        find_flg = False
-        L = len(params)
-
-        for i in range(0, L - 1):
-            for j in range(i + 1, L):
-                if params[i] is params[j]:
-                    grads[i] += grads[j]
-                    find_flg = True
-                    params.pop(j)
-                    grads.pop(j)
-
-                elif params[i].ndim == 2 and params[j].ndim == 2 and \
-                        params[i].T.shape == params[j].shape and np.all(params[i].T == params[j]):
-                    grads[i] += grads[j].T
-
-                    find_flg = True
-                    params.pop(j)
-                    grads.pop(j)
-
-                if find_flg:
-                    break
-            if find_flg:
-                break
-
-        if not find_flg:
-            break
-
-    return params, grads
 
 def sigmoid(x):
     out = 1. / (1. + np.exp(-x))
@@ -50,130 +13,170 @@ def create_contexts_target(corpus, window_size=1):
     target = corpus[window_size:-window_size]
     contexts = []
 
-    for idx in range(window_size, len(corpus) - window_size):
+    for idx in range(window_size, len(corpus)-window_size):
         cs = []
         for t in range(-window_size, window_size + 1):
             if t == 0:
                 continue
             cs.append(corpus[idx + t])
         contexts.append(cs)
-
     return contexts, target
 
+class subsampling:
+    def __init__(self, counts, word_to_id, t=1e-05):
+        value = list(counts.values())
+        self.whole_vocab = np.sum(value)
+        self.del_p = {}
+        for (word, f) in counts.items():
+            temp = f / self.whole_vocab
+            id = word_to_id[word]
+            self.del_p[id] = (1 + np.sqrt(temp / t)) * (t / temp)
+
+    def delete_vocab(self, sentence):
+        new_sentence = []
+        for word in sentence:
+            if self.del_p[word] < np.random.rand():
+                continue
+            else:
+                new_sentence.append(word)
+        return new_sentence
+
+class UnigramTable:
+    def __init__(self, vocab, word_to_id, power=0.75):
+        self.table = []
+        for word in vocab.keys():
+            id = word_to_id[word]
+            if id == 0: continue
+            count = int(vocab[word] ** power)
+            self.table.extend([id] * count)
+        self.p_sum = len(self.table)
+        self.table = np.array(self.table)
+
+    def negative_sampling(self, target, num_sample=5):
+        while(1):
+            index = np.random.randint(low=0, high=self.p_sum, size=num_sample)
+            neg_sample = self.table[index]
+            if target in neg_sample:
+                continue
+            else:
+                neg = np.append([target], neg_sample)
+                return neg
 
 class SkipGram:
-    def __init__(self, window_size, hidden_unit): #window_size: the number of input word, hidden_unit : dimension of vector
+    def __init__(self, window_size=5, hidden_unit=300): #window_size: the number of input word, hidden_unit : dimension of vector
         with open('news/vocab.txt', 'rb') as f:
             corpus = pickle.load(f)
         self.word_to_id = corpus[0]
         self.id_to_word = corpus[1]
+        self.vocab = corpus[2]
         self.vocab_size = len(self.word_to_id)
-        self.num_sentence = corpus[2]['<EOS>']
+        self.num_sentence = self.vocab['<EOS>']
         self.dimension = hidden_unit
         self.lr = 0.025
         self.window_size = np.random.randint(1, window_size)
         self.cache = None
-        self.x = None
+        self.subsampling = subsampling(self.vocab, self.word_to_id)
+        self.Unigram = UnigramTable(self.vocab, self.word_to_id)
+        self.W_embedding = 0.01*np.random.randn(self.vocab_size, self.dimension).astype('f')
+        self.W_embedding_b = np.zeros((self.vocab_size, self.dimension)).astype('f')
 
-        self.W_embedding = 0.01 * np.random.randn(self.vocab_size, self.dimension).astype('f')
-        self.W_embedding_b = np.zeros((self.vocab_size - 1, self.dimension)).astype('f')
-
-
-    def forward(self, contexts, center, codes, ways):
+    def forward(self, contexts, target):
         loss = 0
-        cache = [center]
-        self.x = self.W_embedding[center].reshape(1, -1)        #(1, D)
-
+        cache = []
         for context in contexts:
-            code = np.array(codes[context]).reshape(-1, 1)     #(V-1, 1)
-            way = ways[context]                 #(V-1)
-            score = sigmoid(np.dot(self.W_embedding_b[way], self.x.T))             #(V-1, 1)
-            loss -= np.sum(np.log(score + 1e-07))
-            cache.append((code, way, score))
-        self.cache = cache.copy()
-        return loss/len(contexts)
+            x = self.W_embedding[context].reshape(1, -1)
+            negative_sample = self.Unigram.negative_sampling(target, num_sample=5)
+            out = sigmoid(np.dot(x, self.W_embedding_b[negative_sample].T))
+            p_loss = -np.log(out[:, :1] + 1e-07)
+            n_loss = -np.sum(np.log(1 - out[:, 1:] + 1e-07))
+            loss += (p_loss + n_loss)
+            cache.append((x, context, negative_sample, out))
+
+        self.cache = cache
+        return float(loss)/len(contexts)
+
 
     def backward(self, lr):
-        l = len(self.cache) - 1
-        center = self.cache[0]
-        cache = self.cache[1:]
-        dx = np.zeros_like(self.x)
-        for (code, way, score) in cache:
-            dout = (score - code) / l                   #(V-1, 1)
-            dx += np.dot(dout.T, self.W_embedding_b[way])                          #(1, D)
-            dW_out = np.dot(dout, self.x)                          #(v-1, D)
+        for x, context, label, out in self.cache:
+            dout = out.copy()
+            dout[:, :1] -= 1
+            dW_out = np.dot(x.T, dout).T
+            dx = np.dot(dout, self.W_embedding_b[label])
 
-            self.W_embedding_b[way] -= dW_out * lr
-        self.W_embedding[center] -= dx.squeeze() * lr
+            self.W_embedding_b[label] -= dW_out * lr
+            self.W_embedding[context] -= dx.squeeze() * lr
 
         return None
 
+
     def train(self, epoch):
         start = time.time()
-        lr = self.lr
         n = 0
-        with open('./data/huffman_new.txt', 'rb') as hf:
-            (id_to_code, id_to_way) = pickle.load(hf)
 
         for j in tqdm(range(epoch), desc='Epoch'):
             loss = 0
             count = 0
 
-            # for i in tqdm(range(100), desc='Iteration'):
-            #     if i < 10:
-            #         num = '0' + str(i)
-            #     else:
-            #         num = str(i)
-            data = './news/en-00000-of-00100.txt'
-            with open(data, 'rb') as f:
-                text = pickle.load(f)
-
-            for sentence in tqdm(text):
-                n += 1
-                contexts, target = create_contexts_target(sentence, window_size=self.window_size + 1)
-
-
-                for i in range(len(contexts)):
-                    count += 1
-                    loss += self.forward(contexts[i], target[i], id_to_code, id_to_way)
-                    self.backward(lr)
-
-                #lr decay
-                alpha = 1 - n/(self.num_sentence * epoch)
-                if alpha <= 0.0001:
-                    alpha = 0.0001
-
-                lr = self.lr * alpha
+            for i in tqdm(range(100), desc='Iteration'):
+                if i < 10:
+                    num = '0' + str(i)
+                else:
+                    num = str(i)
+                data = './news/news.en-000' + num + '-of-00100'
+                with open(data, 'rb') as f:
+                    text = pickle.load(f)
+                    max_n = len(text)
+                for sentence in text:
+                    n += 1
+                    #subsampling
+                    new_sentence = self.subsampling.delete_vocab(sentence=sentence)
+                    # contexts, target = create_contexts_target(sentence, window_size=self.window_size + 1)
+                    contexts, target = create_contexts_target(new_sentence, window_size=self.window_size + 1)
 
 
-                if count % 10000 == 1:
-                    train_time = (time.time() - start) / 3600
-                    avg_loss = loss/count
-                    print('time: {}, loss : {}'.format(train_time, avg_loss))
-                    print('{} sentence trained!'.format(n))
-                    print('learning rate : {}'.format(lr))
+                    for i in range(len(target)):
+                        # lr decay
+                        alpha = 1 - n / (self.num_sentence * epoch)
+                        if alpha <= 0.0001:
+                            alpha = 0.0001
+                        lr = self.lr * alpha
 
-                    count = 0
-                    loss = 0
+                        #train
+                        count += 1
+                        loss += self.forward(contexts[i], target[i])
+                        self.backward(lr)
+                        if count % 10000 == 0:
+                            train_time = (time.time() - start) / 3600
+                            avg_loss = loss/count
+                            print('time: {}, loss : {}'.format(train_time, avg_loss))
+                            print('{}/{} sentence trained!'.format(n, max_n))
+                            print('learning rate : {}'.format(lr))
+
+                            count = 0
+                            loss = 0
 
 
         print('Train Finished!')
         print('Train time : {}'.format(time.time()-start))
 
-        with open('data/embedding_sg_hf.pkl', 'wb') as f:
+        with open('data/embedding_sg_neg5.pkl', 'wb') as f:
             pickle.dump(self.W_embedding, f)
         return None
 
-#
-#
-# # #test
-# save_test(file='./data/test.txt', word_to_id=word_to_id)
-model = SkipGram(window_size=5, hidden_unit=300)
-model.train(epoch=1)
+model = SkipGram()
+model.train(1)
+# file = 'data/test.txt'
+# with open('news/vocab.txt', 'rb') as v:
+#     (word_to_id, id_to_word, vocab) = pickle.load(v)
 
-# with open('./data/embedding_sg_hf.pkl', 'rb') as f:
-#     W_embedding = pickle.load(f)
-accuracy = accuracy(file='./news/test_labeled.pkl', W=model.W_embedding)
-print(accuracy[0], accuracy[1])
+#     test = make_test(file)
+#     save_test(file, word_to_id)
 #
-# print(most_similar('king', W_embedding, word_to_id, id_to_word, top=5))
+# with open('data/embedding_sg_neg5.pkl', 'rb') as f:
+#     W_embedding = pickle.load(f)
+# with open('news/vocab.txt', 'rb') as v:
+    # (word_to_id, id_to_word, vocab) = pickle.load(v)
+# most_similar(word='king', W=model.W_embedding, word_to_id=word_to_id, id_to_word=id_to_word)
+# accuracy = accuracy(file='./news/test_labeled.pkl', W=W_embedding)
+# print(accuracy)
+# (0.07812908283250589, 0.1338573575309213, 0.11676550729283539) 1iter
